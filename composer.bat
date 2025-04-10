e79beae1956b0bee3d7d3549896a492ea362f5de
va1holl <pezzatini@proton.me>
2025-04-10 14:28:36 +0300

    Initial commit

---
+++ composer.bat
@@ -0,0 +1,1 @@
+@php "C:\ProgramData\ComposerSetup\composer.phar" %*
---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+
+$conn = pg_connect(
+    "host=$host 
+    port=$port 
+    dbname=$dbname 
+    user=$user 
+    password=$password");
+
+if (!$conn) {
+    die("Connection failed: " . pg_last_error());
+}
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['note'])) {
+    $note = pg_escape_string($conn, $_POST['note']);
+
+
+    $query = "INSERT INTO notes (content) VALUES ('$note')";
+    $result = pg_query($conn, $query);
+
+    if ($result) {
+        echo "<p>+</p>";
+    } else {
+        echo "<p>ошибка: " . pg_last_error() . "</p>";
+    }
+}
+
+
+$query_select_notes = "SELECT * FROM notes ORDER BY created_at DESC LIMIT 10";
+$result_select_notes = pg_query($conn, $query_select_notes);
+
+if (!$result_select_notes) {
+    echo "<p>Ошибка при получении заметок: " . pg_last_error() . "</p>";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Заметки</title>
+</head>
+<body>
+<h1>Добавить заметку</h1>
+<form action="" method="POST">
+    <textarea name="note" rows="4" cols="50" placeholder="Введите вашу заметку"></textarea><br><br>
+    <input type="submit" value="Сохранить">
+</form>
+
+<h2>Сохраненные заметки</h2>
+<?php
+
+if (pg_num_rows($result_select_notes) > 0) {
+    while ($row = pg_fetch_assoc($result_select_notes)) {
+        echo "<div><strong>" . htmlspecialchars($row['created_at']) . "</strong><br>";
+        echo htmlspecialchars($row['content']) . "</div><hr>";
+    }
+} else {
+    echo "<p>Пусто</p>";
+}
+
+
+pg_close($conn);
+?>
+</body>
+</html>
